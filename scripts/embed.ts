45bf76f89b44b992bd37dda57057343730878034
ashishsavani <avsavani@gmail.com>
2023-08-20 00:15:31 -0700

    finally working

--- scripts/embed.ts
+++ scripts/embed.ts
@@ -4,29 +4,89 @@ import {createClient} from "@supabase/supabase-js";
 import fs from "fs";
 import {Configuration, OpenAIApi} from "openai";
 import process from "process";
+import winston from 'winston';
 
 loadEnvConfig("");
 
-const generateEmbeddings = async (essays: HLVideo[]) => {
-    const configuration = new Configuration({apiKey: process.env.OPENAI_API_KEY});
-    const openai = new OpenAIApi(configuration);
+// Create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
 
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    }));
+}
+
+// Return unique key for each chapter
+const getUniqueKey = (videoId: string, chapterTitle: string) : string => `${videoId}-${chapterTitle}`;
+
+// Check if the chapter has already been processed
+const hasProcessedChapter = (processedChapters: Set<string>, videoId: string, chapterTitle: string): boolean => {
+    const key = getUniqueKey(videoId, chapterTitle);
+    return processedChapters.has(key);
+};
+
+// Save the processed chapters
+const saveProcessedChapter = (processedChapters: Set<string>, videoId: string, chapterTitle: string) : void => {
+    const key = getUniqueKey(videoId, chapterTitle);
+    processedChapters.add(key);
+    fs.writeFileSync("processedChapters.json", JSON.stringify(Array.from(processedChapters)));
+};
+
+// Load the processed chapters from storage
+// Load the processed chapters from storage
+const loadProcessedChapters = (): Set<string> => {
+    let data;
+    try {
+        if(fs.existsSync("processedChapters.json")){
+            data = fs.readFileSync("processedChapters.json", 'utf8');
+            return new Set(JSON.parse(data));
+        }
+        else{
+            fs.writeFileSync("processedChapters.json", JSON.stringify([]));
+            return new Set();
+        }
+    } catch (err) {
+        logger.error(`Error reading or writing processedChapters.json: ${err}`);
+        process.exit(1);
+    }
+};
+
+let greenStart = "\x1b[32m";
+let redStart = "\x1b[31m";
+let colorEnd = "\x1b[0m";
+
+const truncate = (str: string, n: number) => (str.length > n) ? str.substr(0, n-1) + '...' : str;
+
+
+const generateEmbeddings = async (essays: HLVideo[]) : Promise<void> => {
+    const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+    const openai = new OpenAIApi(configuration);
     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+
+    const processedChapters: Set<string> = loadProcessedChapters();
 
     for (let i = 0; i < essays.length; i++) {
         const section = essays[i];
-
         for (let j = 0; j < section.chapters.length; j++) {
             const chapter = section.chapters[j];
+            const { video_title, chapter_title, video_date, video_id, start_time, end_time, conversation, conversation_length, conversation_tokens } = chapter;
 
-            const { video_title,chapter_title, video_date, video_id, start_time, end_time, conversation, conversation_length, conversation_tokens } = chapter;
-            // turn conversation into a string with newlines and speaker names
-            // export type HLSegment = {
-            //     start: number; // float in seconds
-            //     end: number;
-            //     speaker: string;
-            //     segment: string;
-            // }
+            const key = getUniqueKey(video_id, chapter_title);
+
+            // save the attempted chapters even if it was not successful
+            if(hasProcessedChapter(processedChapters, video_id, chapter_title)){
+                logger.info(`Skipping processed chapter Video:${truncate(video_title, 30)} chapter ${truncate(chapter_title, 30)}...`);
+                continue;
+            }
+
+            saveProcessedChapter(processedChapters, video_id, chapter_title);
 
             const conversationString = conversation.map(({speaker, segment}) => `${speaker}: ${segment}`).join("\n");
 
@@ -36,6 +96,7 @@ const generateEmbeddings = async (essays: HLVideo[]) => {
             });
 
             const [{embedding}] = embeddingResponse.data.data;
+
             const {data, error} = await supabase
                 .from("chapters")
                 .insert({
@@ -53,12 +114,13 @@ const generateEmbeddings = async (essays: HLVideo[]) => {
                 .select("*");
 
             if (error) {
-                console.log("error 1", error);
+                logger.error(`${redStart}Failed to save video ${truncate(video_title, 30)} chapter ${truncate(chapter_title, 30)}:${colorEnd} ${error.message}`);
             } else {
-                console.log("saved", i, j);
+                logger.info(`${greenStart}Successfully processed video ${truncate(video_title, 30)} chapter ${truncate(chapter_title,
+                    30)}${colorEnd}`);
+                saveProcessedChapter(processedChapters, video_title, chapter_title);
             }
-
-            await new Promise((resolve) => setTimeout(resolve, 200));
+            await new Promise((resolve) => setTimeout(resolve, 20));
         }
     }
 };
--- scripts/merge.py
+++ scripts/merge.py
@@ -31,57 +31,60 @@ def merge_data_to_final_file(summaries_dir, output_path, playlist_path):
         playlist_data = json.load(pl_file)
 
     for filename in os.listdir(summaries_dir):
+        try :
+            if filename.endswith(".json"):
+                with open(os.path.join(summaries_dir, filename), 'r') as file:
+                    video_data = json.load(file)
+                cleaned_filename = remove_special_chars(filename.replace('.json', ''))
+                # print(cleaned_filename)
 
-        if filename.endswith(".json"):
-            with open(os.path.join(summaries_dir, filename), 'r') as file:
-                video_data = json.load(file)
-            cleaned_filename = remove_special_chars(filename.replace('.json', ''))
-            # print(cleaned_filename)
-
-            hlvideo = {
-                "title": filename.replace('.json', ''),
-                "url": "",
-                "date": "",
-                "content": [],
-                "length": 0,
-                "tokens": 0,
-                "chapters": []
-            }
-
-            for i, title in enumerate([video['title'] for video in playlist_data]):
-                if cleaned_filename in remove_special_chars(title):
-                    matching_video = playlist_data[i]
-                    print("Matched:" + title + " ** " + matching_video.get("id", ""))
-                    hlvideo["url"] = matching_video.get("url", "")
-                    hlvideo["date"] = matching_video.get("date", "")
-                    hlvideo["id"] = matching_video.get("id", "")
-
-            # Build the content for the video by merging all chapters
-            for chapter in video_data:
-                # Get all conversation segments as a list
-                chapter_content = [conv["segment"] for conv in chapter["conversations"]]
-                hlvideo["content"].extend(chapter["conversations"])
-
-                chapter_length, chapter_tokens = count_chars_and_tokens(' '.join(chapter_content))
-                hlvideo["length"] += chapter_length
-                hlvideo["tokens"] += chapter_tokens
-
-                hlchapter = {
-                    "video_title": hlvideo["title"],
-                    "chapter_title": chapter["title"],
-                    "video_date": "",
-                    'video_id': hlvideo["id"],
-                    "start_time": chapter['conversations'][0]['start'],
-                    "end_time": chapter['conversations'][-1]['end'],
-                    "conversation": chapter["conversations"],
-                    "conversation_length": chapter_length,
-                    "conversation_tokens": chapter_tokens,
-                    "embedding": []
+                hlvideo = {
+                    "title": filename.replace('.json', ''),
+                    "url": "",
+                    "date": "",
+                    "content": [],
+                    "length": 0,
+                    "tokens": 0,
+                    "chapters": []
                 }
 
-                hlvideo["chapters"].append(hlchapter)
+                for i, title in enumerate([video['title'] for video in playlist_data]):
+                    if cleaned_filename in remove_special_chars(title):
+                        matching_video = playlist_data[i]
+                        print("Matched:" + title + " ** " + matching_video.get("id", ""))
+                        hlvideo["url"] = matching_video.get("url", "")
+                        hlvideo["date"] = matching_video.get("date", "")
+                        hlvideo["id"] = matching_video.get("id", "")
 
-            hljson["videos"].append(hlvideo)
+                # Build the content for the video by merging all chapters
+                for chapter in video_data:
+                    # Get all conversation segments as a list
+                    chapter_content = [conv["segment"] for conv in chapter["conversations"]]
+                    hlvideo["content"].extend(chapter["conversations"])
+
+                    chapter_length, chapter_tokens = count_chars_and_tokens(' '.join(chapter_content))
+                    hlvideo["length"] += chapter_length
+                    hlvideo["tokens"] += chapter_tokens
+
+                    hlchapter = {
+                        "video_title": hlvideo["title"],
+                        "chapter_title": chapter["title"],
+                        "video_date": "",
+                        'video_id': hlvideo["id"],
+                        "start_time": chapter['conversations'][0]['start'],
+                        "end_time": chapter['conversations'][-1]['end'],
+                        "conversation": chapter["conversations"],
+                        "conversation_length": chapter_length,
+                        "conversation_tokens": chapter_tokens,
+                        "embedding": []
+                    }
+
+                    hlvideo["chapters"].append(hlchapter)
+
+                hljson["videos"].append(hlvideo)
+        except Exception as e:
+            print("Exception: " + str(e))
+            continue
 
     # Calculate the overall length and tokens for the entire dataset
     for video in hljson["videos"]:
@@ -95,7 +98,7 @@ def merge_data_to_final_file(summaries_dir, output_path, playlist_path):
 
 if __name__ == "__main__":
     # Call the function
-    summaries_directory = "/Users/ashishsavani/HubermanGPT/data/summaries"
+    summaries_directory = "/Users/ashishsavani/HubermanGPT/data/processed"
     final_output_path = "/Users/ashishsavani/HubermanGPT/data/final_data.json"
 
     merge_data_to_final_file(summaries_directory, final_output_path,
