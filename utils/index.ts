65a97763c47f0183f4a0f066f3478aedfe953a3a
ashishsavani <avsavani@gmail.com>
2023-12-01 12:38:57 -0800

    GPT4 Turbo support

--- utils/index.ts
+++ utils/index.ts
@@ -26,7 +26,7 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
           content: prompt
         }
       ],
-      max_tokens: 150,
+      max_tokens: 1000,
       temperature: 0.0,
       stream: true
     })
